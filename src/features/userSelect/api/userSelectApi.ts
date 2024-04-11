4edc1dae7aeeb3162350412f4ea6deb78d7e27d1
Nikita Benza <nbenza5@gmail.com>
2024-04-11 20:29:15 +0300

    Api configured

--- src/features/userSelect/api/userSelectApi.ts
+++ src/features/userSelect/api/userSelectApi.ts
@@ -2,5 +2,31 @@ import axios from "axios";
 
 const axiosLiveInstance = axios.create({
     withCredentials: true,
-    baseURL: "https://api.bashtel.online/",
+    baseURL: "https://alanbase.vercel.app/api",
 });
+
+
+export const Users = {
+    getUsersList: (page: number, limit: number) => {
+        return axiosLiveInstance.get<GetUsersListResponseType>(`/users?page=${page}&limit=${limit}`)
+    }
+}
+
+export type UserDataType = {
+    id: number;
+    first_name: string;
+    last_name: string;
+    email: string;
+    job?: string;
+}
+
+export type MetaDataType = {
+    from: number;
+    to: number;
+    total: number;
+}
+
+export type GetUsersListResponseType = {
+    data: Array<UserDataType>;
+    meta: MetaDataType
+}
